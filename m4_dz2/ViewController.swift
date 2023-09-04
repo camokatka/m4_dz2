c91f17d070257bbcfd03a9afd0c2cbdb3de8481b
camokatka <elizaveta.serykh@gmail.com>
2023-09-04 09:40:10 +0600

    Initial Commit

---
+++ m4_dz2/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  m4_dz2
+//
+//  Created by Elizabeth Serykh on 04.09.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
